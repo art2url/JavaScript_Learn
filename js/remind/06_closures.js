b548778bd16f5227028211a9426d62d5d7f68912
art2url <artem.turlenko@gmail.com>
2021-07-25 21:13:14 +0300

    upd

--- js/remind/06_closures.js
+++ js/remind/06_closures.js
@@ -8,19 +8,30 @@
 // console.log(helloToBob);
 // helloToBob();
 
-function createFrameworkManager(){
-    const fw = ['Angular','React']
-    return {
-        print: function(){
-            console.log(fw)
-        },
-        add: function(framework){
-            fw.push(framework)
-        }
-    }
-}
+// function createFrameworkManager(){
+//     const fw = ['Angular','React']
 
-const manager = createFrameworkManager();
-manager.print();
-manager.add('VueJs');
-manager.print;
+//     return {
+//         print: function(){
+//             console.log(fw.join(' '))
+//         },
+//         add: function(framework){
+//             fw.push(framework)
+//         }
+//     }
+// }
+
+// const manager = createFrameworkManager();
+// manager.print();
+// manager.add('VueJs');
+// manager.print;
+
+//setTimeout
+const fib = [1, 2, 3, 5, 8, 13];
+for (var i = 0; i < fib.length; i++) {
+    (function(j){
+    setTimeout(function () {
+        console.log(`fib[${j}] = ${fib[j]}`)
+    }, 1500)
+    })(i)
+}
