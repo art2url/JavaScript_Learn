36b383c91c6e172ea508013283755181d4ed1024
art2url <artem.turlenko@gmail.com>
2021-08-10 22:52:10 +0300

    upd

--- js/hackerrank/01_hacker_rank_d2.js
+++ js/hackerrank/01_hacker_rank_d2.js
@@ -1,7 +1,11 @@
-let s = "javascriptloops";
-let arr = s.split('');
-console.log(arr);
-console.log(1+2);
-alert(arr);
+// let s = "javascriptloops";
+// let arr = s.split('');
+// console.log(arr);
+// console.log(1+2);
+// alert(arr);
+
+var amount = 99.99;
+amount = amount * 3;
+console.log(amount.toFixed(2));
 
 
