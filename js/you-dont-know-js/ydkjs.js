4c2212806a26733dbc7bc9769c34509f05ed335a
art2url <artem.turlenko@gmail.com>
2021-09-06 20:02:17 +0300

    Update ydkjs.js

---
+++ js/you-dont-know-js/ydkjs.js
@@ -0,0 +1,18 @@
+let balance = prompt("Type your balance:", 0);
+const TAX_RATE = 1.2;
+const PHONE_PRICE = 9999.99;
+const ACCESSOIR_PRICE = 400;
+let minBalance = 0;
+
+function overallPhonePrice() {
+    return (PHONE_PRICE + ACCESSOIR_PRICE) * TAX_RATE;
+}
+// overallPhonePrice().toFixed(2);
+
+
+if (balance > minBalance) {
+    balance = balance - overallPhonePrice();
+    console.log(+(balance.toFixed(2)));
+} else {
+    console.log("You cannot buy phone");
+}
