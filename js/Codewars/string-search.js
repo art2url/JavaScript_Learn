1bdda878c9288918ede50ff709520d49b603df35
art2url <artem.turlenko@gmail.com>
2021-09-16 12:42:36 +0300

    Update string-search.js

--- js/Codewars/string-search.js
+++ js/Codewars/string-search.js
@@ -1,6 +1,17 @@
-function chars(str1, str2) {
-    for (let char of str1) {
-        let arr = [];
-    }
+//Create a function that accepts 2 string arguments and 
+//returns an integer of the count of occurrences 
+//the 2nd argument is found in the first one.
 
+//If no occurrences can be found, a count of 0 should be returned.
+
+function strCount(str, letter) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) == letter) {
+            count += 1;
+        }
+    }
+    return count
 }
+
+strCount("hello", "l")
