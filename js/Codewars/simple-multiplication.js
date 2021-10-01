f7504b3cef8e637f1d817f2b26e8615392aefa9d
art2url <artem.turlenko@gmail.com>
2021-10-01 12:54:13 +0300

    upd

---
+++ js/Codewars/simple-multiplication.js
@@ -0,0 +1,6 @@
+// This kata is about multiplying a given number 
+// by eight if it is an even number and by nine otherwise.
+
+let simpleMultiplication = (number) => {
+    return (number % 2) ? number * 9 : number * 8
+}
