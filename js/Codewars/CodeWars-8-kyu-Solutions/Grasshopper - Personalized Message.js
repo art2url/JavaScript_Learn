e958e4f0a81c2936a1314a0a5c1327fc971c0fc9
art2url <artem.turlenko@gmail.com>
2021-10-26 18:30:25 +0300

    upd

--- js/Codewars/CodeWars-8-kyu-Solutions/Grasshopper - Personalized Message.js
+++
@@ -1,8 +0,0 @@
-// Create a function that gives a personalized greeting. 
-// This function takes two parameters: name and owner.
-
-// Use conditionals to return the proper message:
-// 1. name equals owner	'Hello boss'
-// 2. otherwise	'Hello guest'
-
-let greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest';
--- js/Codewars/CodeWars-8-kyu-Solutions/String repeat.js
+++
@@ -1,5 +0,0 @@
-// Write a function called repeatStr which 
-// repeats the given string string 
-// exactly n times.
-
-let repeatStr = (n, s) => s.repeat(n);
